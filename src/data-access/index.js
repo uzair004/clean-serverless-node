71e4791f0a630a0862b92fd3004d8b241c245646
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-01-21 22:25:08 +0500

    Update index.js

--- src/data-access/index.js
+++ src/data-access/index.js
@@ -1,22 +1,21 @@
 const AWS = require('aws-sdk');
 const { makeUserDb } = require('./userDb');
 
+const { isValueTrue } = require('../util/util');
+
 const { makeDb } = require('./dynamoDb');
 
-const region = process.env.AWS_ACCOUNT_REGION || 'us-east-1';
+const region = process.env.REGION || 'us-east-1';
 const tableName = process.env.DYNAMODB_MAIN_TABLE || 'main-db-table';
 const IS_OFFLINE = process.env.IS_OFFLINE;
 
 let dynamoDb;
-function makeDbConnect(isOffline = true) {
+function makeDbConnect() {
   if (dynamoDb) {
-    //} && dynamoDb.config.region === region) {
     return dynamoDb;
   }
-  if (
-    isOffline === true && // Used as over-ride
-    (IS_OFFLINE === 'true' || IS_OFFLINE === undefined || isOffline === true)
-  ) {
+
+  if (isValueTrue(IS_OFFLINE)) {
     return new AWS.DynamoDB({
       region: 'localhost',
       endpoint: 'http://localhost:8000',
@@ -35,10 +34,6 @@ function makeDbConnect(isOffline = true) {
   });
   return dynamoDb;
 }
-// Alternative solution:
-// const db = makeDb({ makeDbConnect })
-// const requestDb = makeRequestDb({ db, getTableName })
-// const userDb = makeUserDb({ db, getTableName })
 
 const userDb = makeUserDb({ makeDb, makeDbConnect, getTableName });
 
